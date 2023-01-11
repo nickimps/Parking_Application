34f93069098b20862b00549ca341779d7d8149ff
Jtsang2001 <jtsang@lakeheadu.ca>
2023-01-10 22:34:20 -0500

    Added more login stuff

--- app/src/main/java/com/example/gpsapp/LoginActivity.java
+++ app/src/main/java/com/example/gpsapp/LoginActivity.java
@@ -3,7 +3,11 @@ package com.example.gpsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+//import android.widget.TextView;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -12,13 +16,16 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class LoginActivity extends AppCompatActivity {
 
     FirebaseFirestore firestore;
+    EditText editUsername, editPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        EditText getUsername = (EditText) findViewById(R.id.usernameInsertText);
+        EditText getPassword = (EditText) findViewById(R.id.passwordInsertText);
 
         //Database
         firestore = FirebaseFirestore.getInstance();
@@ -32,6 +39,18 @@ public class LoginActivity extends AppCompatActivity {
 
         //Add constraints
         Button button = findViewById(R.id.loginButton);
-        button.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, MapsActivity.class)));
+        //button.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, MapsActivity.class)));
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+              String username = getUsername.getText().toString();
+              String password = getPassword.getText().toString();
+              System.out.println(username);
+              System.out.println(password);
+              Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+              startActivity((intent));
+            }
+        });
+
+
     }
 }
