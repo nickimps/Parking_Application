7c11186916ff0101b81a915fdf3360ec14cd1400
Jtsang2001 <jtsang@lakeheadu.ca>
2023-01-10 21:08:43 -0500

    added login stuff

--- app/src/main/java/com/example/gpsapp/LoginActivity.java
+++ app/src/main/java/com/example/gpsapp/LoginActivity.java
@@ -1,4 +1,21 @@
 package com.example.gpsapp;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Add constraints
+        Button button = findViewById(R.id.loginButton);
+        button.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, MapsActivity.class)));
+    }
 }
