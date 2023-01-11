7d01ad5021cd26607d44e65c125f7a31f2be9eb1
mltommas <92130846+mltommas@users.noreply.github.com>
2023-01-10 22:17:36 -0500

    Connecting firebase

--- app/src/main/java/com/example/gpsapp/LoginActivity.java
+++ app/src/main/java/com/example/gpsapp/LoginActivity.java
@@ -2,17 +2,33 @@ package com.example.gpsapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.widget.Button;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        //Database
+        firestore = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("firstName", "Mason");
+        user.put("lastName", "Tommasini");
+        user.put("Description", "AWESOME EPIC MODE HEHE");
+
+        //firestore.collection("Users").add(user);
+        firestore.collection("Users").document("Mason Info").set(user);
 
         //Add constraints
         Button button = findViewById(R.id.loginButton);
