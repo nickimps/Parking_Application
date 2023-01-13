5b8588b074ef598a741e29bad5f0eef8fc171f77
Jtsang2001 <jtsang@lakeheadu.ca>
2023-01-13 17:09:21 -0500

    Added registration screen

--- app/src/main/java/com/example/gpsapp/RegisterActivity.java
+++ app/src/main/java/com/example/gpsapp/RegisterActivity.java
@@ -7,12 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button register = findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                //Map<String, Object> user = new HashMap<>();
+                //user.put("username", username);
+                //user.put("password", password);
+
+                //firestore.collection("Users").add(user);
+               //firestore.collection("Users").add(user);
+
+                Intent intent = new Intent(RegisterActivity.this, MapsActivity.class);
+            }}
+        );
+
+
 
         Button goBack = findViewById(R.id.backToLogin);
         goBack.setOnClickListener(new View.OnClickListener() {
