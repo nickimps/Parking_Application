378a671ad0dee82a2bd59459fad9ec70baef1215
Nicholas Imperius <nimperius200@gmail.com>
2023-01-19 23:10:04 -0500

    Overhauled UI for Login Screen + functionality still works

--- app/src/main/java/com/example/gpsapp/LoginActivity.java
+++ app/src/main/java/com/example/gpsapp/LoginActivity.java
@@ -4,17 +4,29 @@ import static android.content.ContentValues.TAG;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
     FirebaseFirestore firestore;
-    private EditText getUsername, getPassword;
+    private TextInputEditText getUsername, getPassword;
 
     //Fetch the stored information when the app is loaded, this function is called when the app is opened again
     @Override
@@ -84,9 +96,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // REGISTER CLICKABLE TEXT
+        createClickableRegisterText();
+
         // Store the ids of the edit text fields
-        getUsername = findViewById(R.id.usernameInsertText);
-        getPassword = findViewById(R.id.passwordInsertText);
+        getUsername = findViewById(R.id.usernameTextInputEditText);
+        getPassword = findViewById(R.id.passwordTextInputEditText);
 
         // LOGIN BUTTON
         Button logButton = findViewById(R.id.loginButton);
@@ -120,13 +135,40 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     });
         });
+    }
 
-        // REGISTER BUTTON
-        Button regButton = findViewById(R.id.goToRegisterButton);
-        regButton.setOnClickListener(v -> {
-            // Send the user to the register screen.
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity((intent));
-        });
+    /**
+     * Create a clickable textview to bring the user to the register screen
+     */
+    public void createClickableRegisterText() {
+
+        // Get id of textview and save its default string
+        TextView textView = findViewById(R.id.registerTextView);
+        String text = "Don't have an account? Register here";
+
+        // Create spannable string
+        SpannableString ss = new SpannableString(text);
+
+        // Create listener for the onClick
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                ds.setColor(ds.linkColor);
+                ds.setUnderlineText(false);
+            }
+            @Override
+            public void onClick(@NonNull View view) {
+                // Go to register screen on click
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity((intent));
+            }
+        };
+
+        // Set which part is clickable (just the register here part)
+        ss.setSpan(clickableSpan, 23,36, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Set the textview to the new clickable text
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
