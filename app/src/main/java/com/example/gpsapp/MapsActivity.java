df0828cd840e9253738b51d1f366e23050c0b648
Jtsang2001 <jtsang@lakeheadu.ca>
2023-03-14 22:30:10 -0400

    Updated some minor details, rearranged some functions

--- app/src/main/java/com/example/gpsapp/MapsActivity.java
+++ app/src/main/java/com/example/gpsapp/MapsActivity.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
